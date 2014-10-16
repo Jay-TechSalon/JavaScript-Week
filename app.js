564ce67d9310a59f577960bfcee80df7d16808aa
Jay Sweet <jaydiran@gmail.com>
2014-10-15 23:10:56 -0700

    Add comments to the files and basic refactor. Also added feature that allows user to upload personal books to their library.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// constructor function
+// constructor function for pre-selected top reads
 function Book(name, rating, photo, review){
   this.name = name;
   this.rating = rating;
@@ -9,7 +9,7 @@ function Book(name, rating, photo, review){
 
 var infoBox = document.getElementById('info');
 
-// new books
+// new books added to the DOM
 var fourBook = new Book('The Four Agreements', 4.07, 'http://d.gr-assets.com/books/1348204644l/6596.jpg','In The Four Agreements, don Miguel Ruiz reveals the source of self-limiting beliefs that rob us of joy and create needless suffering. Based on ancient Toltec wisdom, the Four Agreements offer a powerful code of conduct that can rapidly transform our lives to a new experience of freedom, true happiness, and love. - from GoodReads Reviews' );
 var secretBook = new Book('The Secret', 3.53, 'http://d.gr-assets.com/books/1391995828l/52529.jpg','Fragments of a Great Secret have been found in the oral traditions, in literature, in religions and philosophies throughout the centuries. For the first time, all the pieces of The Secret come together in an incredible revelation that will be life-transforming for all who experience it. - from GoodReads Reviews' );
 
@@ -17,7 +17,7 @@ var secretBook = new Book('The Secret', 3.53, 'http://d.gr-assets.com/books/1391
 function displayBook() {
 
   var bookReview = document.createElement('div');
-  bookReview.className = 'book_review cf';
+  bookReview.className = 'book-review cf';
 
   var result = "<h2>Book: " + this.name + "</h2>";
   result += "<p>Rating: " + this.rating + " out 5.</p>";
@@ -27,10 +27,5 @@ function displayBook() {
   infoBox.appendChild(bookReview);
 }
 
-
 fourBook.displayBook();
 secretBook.displayBook();
-
-//////////////////
-
-
--- library.js
+++ library.js
@@ -1,9 +1,13 @@
-function UsersBook(name, rating, review){
+// constructor function for users books
+function UsersBook(name, image, rating, review){
   this.name = name;
+  this.image = image;
   this.rating = rating;
   this.review = review;
 }
 
+// for loop to iterate throught the added books to get
+// the average rating and total books added
 function getAverage() {
     var bookTotal = userList.length,
         ratingSum = 0,
@@ -16,27 +20,31 @@ function getAverage() {
     document.getElementById("book-total").innerHTML = bookTotal;
 }
 
-
+// save the users books to an array
 var userList = [];
 var i = 0;
-
 
 var bookButton = document.getElementById('add-my-book');
 bookButton.addEventListener("click", addMyBook);
 
+// add book function that grabs the values from the users input
+// and logs it into the userList array above as it instantiates
+// another book - then adds this to the DOM
 function addMyBook() {
   var name = document.getElementById('my-book-name').value;
+  var image = document.getElementById('my-image').value;
   var rating = document.getElementById('my-book-rating').value;
   var review = document.getElementById('my-book-notes').value;
 
-  userList[i] = new UsersBook(name, rating, review);
+  userList[i] = new UsersBook(name, image, rating, review);
 
   var myBookCollection = document.getElementById('my-book-collection');
   var bookContainer = document.createElement('div');
-  bookContainer.className = 'our_review';
+  bookContainer.className = 'our_review cf';
 
   var result = '<h2>Book: ' + userList[i].name + '</h2>';
       result += '<p>Rating: ' + userList[i].rating + ' out 5.</p>';
+      result += "<img src='" + userList[i].image + "'/>";
       result += '<p>Notes: ' + userList[i].review + '</p>';
 
   bookContainer.innerHTML = result;
